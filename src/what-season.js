6839e6790bb200f5f1a7d562fa1e2b741d24dd18
dashaborzova <dashaborzova@yandex.ru>
2023-12-22 22:40:00 +0300

    feat: pass 38 tasks

--- src/what-season.js
+++ src/what-season.js
@@ -12,26 +12,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  let season = ['spring', 'summer', 'autumn', 'winter' ];
+  // let season = ['spring', 'summer', 'autumn', 'winter' ];
   let month = date.getMonth();
-  switch(month){
-    case 12:
-    case 1:
-    case 2:
-      return season[3];
-    case 3:
-    case 4:
-    case 5:
-      return season[0];
-    case 6:
-    case 7:
-    case 8:
-      return season[1];
-    case 9:
-    case 10:
-    case 11:
-      return season[2];    
-  }
+  if (month === 11 || month === 0 || month === 1) return 'winter';
+  if (month === 2 || month === 3 || month === 4) return 'spring';
+  if (month === 5 || month === 6 || month === 7) return 'summer';
+  else return 'autumn';
 }
 
 module.exports = {
